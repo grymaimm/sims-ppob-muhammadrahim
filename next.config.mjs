0cd6cf1de387bad3224771c05d296abdc6c4601f
grimaim <muhammadrahim.mr196@gmail.com>
2025-08-01 13:08:49 +0700

    ♻️ refactor(next): use remotePatterns for image domains
    - migrate from domains to remotePatterns for improved image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'minio.nutech-integrasi.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'minio.nutech-integrasi.com',
+      },
+    ],
   },
 };
 
